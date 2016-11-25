d432ac2424a3a25a7275edfd13b0d77949634402
zq <114165396@qq.com>
2016-11-25 10:54:44 +0800

    update

---
+++ src/js/navigator.js
@@ -0,0 +1,11 @@
+const navigator = require('@weex-module/navigator')
+const history = require('./js/history')
+module.exports = {
+  push: function(url, options){
+
+  },
+
+  pop: function(url){
+
+  }
+}
